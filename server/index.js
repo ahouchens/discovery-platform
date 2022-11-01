f16ec7c65f8b9832b2ced0357ea707a714b7ab91
ahouchens <webtechalex@gmail.com>
2022-11-01 16:22:52 -0400

    Add path var

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
-
+const path = require("path");
 const app = express();
 
 // Have Node serve the files for our built React app
